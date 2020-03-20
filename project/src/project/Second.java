04d2f3912fabfdaf344fee649443621a4ae98335
karthiktr147 <61529403+karthiktr147@users.noreply.github.com>
2020-03-20 09:21:07 +0530

    Update and rename First.java to Second.java

--- project/src/project/Second.java
+++ project/src/project/Second.java
@@ -3,7 +3,7 @@ package project;
 public class First {
 
 	public static void main(String[] args) {
-		System.out.println("Wewlcome to possible");
+		System.out.println("Welcome to possible");
 		System.out.println("hii");
 		System.out.println("This is the third change");
 
